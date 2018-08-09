fa984d8d25524324c835eb5e37ca6f3b37e150c7
d0man <jakub.tomasz.domanski@gmail.com>
2018-08-09 21:04:14 +0200

    feat: Added css to make site rwd and grid looking

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiZG9tYW4iLCJhIjoiY2praDg0ZnZrMHBpODNxanlpdHJwb2hjNSJ9.kjjsJ7q2-DaIbxgNiA8txw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
